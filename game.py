b7c2b9a777dd3dfd204061b5598e10e4519bd644
Jakub Stefan <kejkun1992@gmail.com>
2018-08-14 21:34:27 +0200

    class Graphics contains all graphics

---
+++ game.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import time, random
+from tkinter import *
+
+'''this class contains all game graphics'''
+
+
+class Graphics():
+    def __init__(self):
+        self.left = PhotoImage(file='graphics/left.png')
+        self.right = PhotoImage(file='graphics/right.png')
+        self.forward = PhotoImage(file='graphics/forward.png')
+        self.wall = PhotoImage(file='graphics/wall.png')
+        self.left_right = PhotoImage(file='graphics/left_right.png')
+        self.forward_left = PhotoImage(file='graphics/left_forward.png')
+        self.forward_right = PhotoImage(file='graphics/forward_right.png')
+        self.forward_left_right = PhotoImage(file='graphics/forward_left_right.png')
+
+
